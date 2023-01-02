94bcd0b2a52bf58aecfae7bcc88bb39653aa087f
magapalacios <gabipalacios2412@gmail.com>
2023-01-02 20:55:43 -0300

    nuevos cambios

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-  console.log("Servidor corriendo");
+
+const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Se prendió en el puerto ${PORT}`);
+ 
 });
 
 app.get("/", (req, res) => {
